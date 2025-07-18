7cd5fd1fe352009cab5724a1bd3d39d239810d0c
Khalid Abdelrazk <khalidabdelrazk4@gmail.com>
2025-07-18 22:25:56 +0300

    - fix the selected tab not changed now full ui finished

--- lib/features/product tab/ui/product_tab.dart
+++ lib/features/product tab/ui/product_tab.dart
@@ -1,9 +1,11 @@
 import 'package:ecommerce/core/di/di.dart';
 import 'package:ecommerce/core/utils/app_colors.dart';
+import 'package:ecommerce/features/product%20tab/ui/cubit/product_tab_states.dart';
 import 'package:ecommerce/features/product%20tab/ui/cubit/product_tab_view_model.dart';
 import 'package:ecommerce/features/product%20tab/ui/widgets/category_items.dart';
 import 'package:ecommerce/features/product%20tab/ui/widgets/category_tabs.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ProductTab extends StatefulWidget {
@@ -17,22 +19,36 @@ class _ProductTabState extends State<ProductTab> {
   final ProductTabViewModel productTabViewModel = getIt<ProductTabViewModel>();
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.whiteColor,
-      body: Row(
-        children: [
-          CategoryTabs(
-            categories: productTabViewModel.categories,
-            selectedCategory: productTabViewModel.selectedCategory,
-            onCategorySelected: (category) {
-              setState(() => productTabViewModel.selectedCategory = category);
-              productTabViewModel.fetchItems(category);
-            },
-          ),
-          CategoryItems(items: productTabViewModel.items),
-        ],
-      ),
-    );
+  void initState() {
+    super.initState();
+    // Fetch initial items for the default category
+    productTabViewModel.fetchItems(productTabViewModel.selectedCategory);
   }
+
+  @override
+Widget build(BuildContext context) {
+  return BlocProvider(
+    create: (context) => productTabViewModel,
+    child: Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      body: BlocBuilder<ProductTabViewModel, ProductTabStates>(
+        builder: (context, state) {
+          return Row(
+            children: [
+              CategoryTabs(
+                categories: productTabViewModel.categories,
+                selectedCategory: productTabViewModel.selectedCategory,
+                onCategorySelected: (category) {
+                  productTabViewModel.selectCategory(category);
+                },
+              ),
+              CategoryItems(items: productTabViewModel.items),
+            ],
+          );
+        },
+      ),
+    ),
+  );
+}
+
 }
